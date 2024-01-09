1570610fa4be05df88b9d3788ebb785bc47c6e44
Dominic Rivest <domrivest@outlook.com>
2024-01-08 21:11:18 -0500

    Commit Initial

---
+++ app.py
@@ -0,0 +1,134 @@
+from dash import Dash, html, dcc, callback, Output, Input, State, dash_table
+import dash_daq as daq
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+import base64
+import datetime
+from classesIET import figureIET
+
+
+df_colors = pd.read_csv('assets/colors.csv')
+colordict = dict(zip(df_colors.variable, df_colors.color))
+frDict = dict(zip(df_colors.variable, df_colors.label_fr))
+
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
+
+app.layout = html.Div([
+    html.H1(
+        children='Outil de génération de graphiques - IET',
+        style={
+            'textAlign': 'center',
+        }
+    ),
+    html.Div([
+        daq.ToggleSwitch(
+            id='isFrenchToggle',
+            value=False,
+            label='Produire les graphiques en Français',
+            labelPosition='top'
+        ),
+        daq.ToggleSwitch(
+            id='dimensionsToggle',
+            value=True,
+            label='Contraindre les dimensions des graphiques à 1000x400 pixels',
+            labelPosition='top'
+        ),
+        daq.ToggleSwitch(
+            id='sourceToggle',
+            value=False,
+            label="Afficher la source lorsqu'elle est renseignée dans le fichier",
+            labelPosition='top'
+        ),
+        daq.ToggleSwitch(
+            id='showTitle',
+            value=False,
+            label="Afficher le titre lorsqu'il est renseigné dans le fichier",
+            labelPosition='top'
+        ),
+        dbc.Button("Effacer les graphiques", id='boutonEffacer', color="primary", n_clicks=0),
+    ], style={'display': 'flex', 'justifyContent': 'space-between', 'padding': 10}),
+    dcc.Upload(
+        id='upload-data',
+        children=html.Div([
+            'Drag and Drop or ',
+            html.A('Select Files')
+        ]),
+        style={
+            'width': '100%',
+            'height': '60px',
+            'lineHeight': '60px',
+            'borderWidth': '1px',
+            'borderStyle': 'dashed',
+            'borderRadius': '5px',
+            'textAlign': 'center',
+            'margin': '10px'
+        },
+        # Allow multiple files to be uploaded
+        multiple=True
+    ),
+    html.Div(id='output-data-upload'),
+])
+
+def parse_contents(contents, filename, date, isFrench, isDim, isSource, showTitle):
+    content_type, content_string = contents.split(',')
+
+    decoded = base64.b64decode(content_string)
+    try:
+        if 'txt' in filename:
+            graph = figureIET(decoded, colordict, frDict, isFrench, isDim, isSource, showTitle)
+    except Exception as e:
+        print(e)
+        return html.Div([
+            'There was an error processing this file.'
+        ])
+
+    return dcc.Graph(id=str([filename, date]), figure=graph.fig)
+    #     html.Div([
+    #     html.H5(filename),
+    #     html.H6(datetime.datetime.fromtimestamp(date)),
+
+    #     dash_table.DataTable(
+    #         graph.df.to_dict('records'),
+    #         [{'name': i, 'id': i} for i in graph.df.columns]
+    #     ),
+
+    #     html.Hr(),  # horizontal line
+
+    #     # For debugging, display the raw contents provided by the web browser
+    #     html.Div('Raw Content'),
+    #     html.Pre(contents[0:200] + '...', style={
+    #         'whiteSpace': 'pre-wrap',
+    #         'wordBreak': 'break-all'
+    #     })
+    # ]), dcc.Graph(id=str(date), figure=graph.fig)
+
+@callback(Output('output-data-upload', 'children', allow_duplicate=True),
+              Input('upload-data', 'contents'),
+              State('upload-data', 'filename'),
+              State('upload-data', 'last_modified'),
+              State('isFrenchToggle', 'value'),
+              State('dimensionsToggle', 'value'),
+              State('sourceToggle', 'value'),
+              State('showTitle', 'value'),
+              prevent_initial_call = True)
+def update_output(list_of_contents, list_of_names, list_of_dates, isFrench, isDim, isSource, showTitle):
+    if list_of_contents is not None:
+        children = [
+            parse_contents(c, n, d, isFrench, isDim, isSource, showTitle) for c, n, d in
+            zip(list_of_contents, list_of_names, list_of_dates)]
+        return children
+    
+@callback(
+    Output('output-data-upload', 'children'),
+    Input('boutonEffacer', 'n_clicks'),
+    prevent_initial_call=True
+)
+def update_output(n_clicks):
+    return None
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ classesIET.py
@@ -0,0 +1,188 @@
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import plotly.io as pio
+import numpy as np
+from io import StringIO  
+import chardet
+
+class figureIET:
+  def __init__(self, decoded, colordict, frDict, isFrench, isDim, isSource, showTitle):
+
+   # Plotly default theme
+   pio.templates.default = "plotly_white"
+      
+   the_encoding = chardet.detect(decoded)['encoding']
+   try:   
+      file = decoded.decode(the_encoding.lower())   
+   except:
+      print("Le décodage ne fonctionne pas")
+
+   header_dict = {}
+   header_height = 0
+   # Split the header into individual lines
+   for line in file.splitlines():
+      # Split each line into components and remove \n
+      line = line.replace('\n', '')
+      components = line.split(';')
+      
+      if components[0] == 'metadata':
+         header_height = header_height + 1
+
+      # Remove empty components
+      components = list(filter(None, components))
+
+      if len(components) == 3 and components[0] == 'metadata':
+            key = components[1]
+            value = components[2]
+
+            # Add the key-value pair to the dictionary
+            header_dict[key] = value
+        
+   df = pd.read_csv(StringIO(file), encoding=the_encoding.lower(), header=header_height, sep=';')
+   df = df.drop(['data'], axis=1)
+
+   # Ajouts des valeurs à la classe
+   self.metadataDict = header_dict
+   self.df = df
+
+   match self.metadataDict['chart.type']:
+      case 'area':
+         self.fig=px.area(self.df, x=self.df.columns[0], y=self.df.columns, color_discrete_map=colordict)
+         self.fig.for_each_trace(lambda trace: trace.update(fillcolor = trace.line.color))
+         self.fig.update_annotations()
+         #self.fig.update_xaxes(type='category')
+
+      case 'bar.grouped.horizontal':
+         self.df[self.df.columns[1]] = self.df[self.df.columns[1]].astype('str')
+         self.fig = px.bar(self.df, y=self.df.columns[0], x=self.df.columns, color=df.columns[1], color_discrete_map=colordict, barmode='group', orientation='h')
+
+      case 'bar.grouped.stacked':
+      #   if self.df.iloc[0 ,1] == '-':
+      #     # Création de subplots
+      #     subplotsIndex = df[df.columns[0]].unique().tolist()
+      #     self.fig = make_subplots(rows = 1, cols = len(subplotsIndex), subplot_titles=subplotsIndex)
+      #     self.fig.update_layout(barmode = "stack")
+      #     self.fig.update_xaxes(type='category')
+      #     for i in subplotsIndex:
+      #        dfr = df[df[df.columns[0]] == i].transpose()
+      #        self.fig.add_trace(
+      #           go.Bar(
+      #             x = dfr.iloc[1, :], y=dfr.iloc[2:, :]
+      #           ),
+      #        row = 1, col = subplotsIndex.index(i)+1)
+      #   else:
+         self.fig = px.bar(self.df, x=self.df.columns[1], y=self.df.columns, facet_col=self.df.columns[0], color_discrete_map=colordict)
+         self.fig.update_xaxes(type='category')
+         self.fig.update_xaxes(matches=None)
+         self.fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
+         for axis in self.fig.layout:
+            if type(self.fig.layout[axis]) == go.layout.XAxis:
+               self.fig.layout[axis].title.text = ''
+         i = 0#len(df[df.columns[0]].unique().tolist())-1 # Avoir le nombre d'index (années) différentes
+         # for i in df[df.columns[0]].unique().tolist():
+         #       if i == min(df[df.columns[0]].unique().tolist()):
+         #          self.fig.layout['xaxis'].title.text = str(i)
+         #          self.fig.layout['xaxis'].title.visible = True
+         #       else:
+         #          self.fig.layout['xaxis'+str(df[df.columns[0]].unique().tolist().index(i)+1)].title.text = str(i)
+
+
+      case 'bar.stacked':
+         self.fig = px.bar(self.df, x=self.df.columns[0], y=self.df.columns, color_discrete_map=colordict, barmode='stack')
+         self.fig.update_xaxes(type='category')
+         
+      case 'bar.grouped':
+         self.fig = px.bar(self.df, x=self.df.columns[0], y=self.df.columns[2], color=self.df.columns[1], color_discrete_map=colordict, barmode='group')
+         self.fig.update_xaxes(type='category')
+
+      case 'line':
+         self.fig = px.line(self.df, x=self.df.columns[1], y=self.df.columns[2], color=self.df.columns[0], color_discrete_map=colordict, markers=True)
+
+      case 'scatter':
+         self.fig = px.scatter(self.df, x=self.df.columns[1], y=self.df.columns[2], color=self.df.columns[0], color_discrete_map=colordict)
+
+      case 'pie':
+         #self.df[self.df.columns[1]] = self.df[self.df.columns[1]].apply(lambda x: x.replace(' ', '')) # Si espace 
+         self.df[self.df.columns[1]] = self.df[self.df.columns[1]].astype('float')
+         if isFrench:
+            frList=[frDict[k] for k in df[df.columns[0]].tolist() if k in frDict]
+            self.df['namesFr'] = frList
+            self.fig = px.pie(self.df, values=self.df.columns[1], names='namesFr', color_discrete_map=colordict)
+            self.fig.update_layout(legend_title = 'Légende')
+         else:
+            self.fig = px.pie(self.df, values=self.df.columns[1], names=self.df.columns[0], color_discrete_map=colordict)
+
+      case _:
+         print("Ce type de graphique n'est pas défini")
+   
+   # Si français - Changer les noms de variables
+   if isFrench and not self.metadataDict['chart.type'] == 'pie':
+      self.fig.for_each_trace(lambda t: t.update(name = frDict[t.name],
+                                      legendgroup = frDict[t.name],
+                                      hovertemplate = t.hovertemplate.replace(t.name, frDict[t.name])
+                                     )
+         )
+      self.fig.update_layout(legend_title = 'Légende')
+   else:
+      self.fig.update_layout(legend_title = 'Legend')
+
+   # Ajouts des noms d'axes s'ils existent
+   try:
+      self.fig.update_layout(yaxis_title = self.metadataDict['chart.yLabel'])
+   except:
+      self.fig.update_layout(yaxis_title = "")
+   try:
+      self.fig.update_layout(xaxis_title = self.metadataDict['chart.xLabel'])
+   except:
+      self.fig.update_layout(xaxis_title = "")
+
+   # Contraindre les dimensions is isDim est vrai
+   if isDim:
+      self.fig.update_layout(
+         width = 1000,
+         height = 400
+      )
+
+   # Montrer le titre si il est renseigné
+   if showTitle:
+      try:
+         self.fig.update_layout(title_text = self.metadataDict['chart.title'], title_x=0.5)
+      except:
+         print("Cette figure n'a pas de titre")
+
+   # Montrer la source si elle est renseignée
+   if isSource:
+      try:
+         self.fig.update_layout(title = self.metadataDict['chart.title'])
+         # HOW TO ADD A FOOTNOTE TO BOTTOM LEFT OF PAGE
+         self.fig.add_annotation(
+            text = (f"Source: {self.metadataDict['chart.source']}")
+            , showarrow=False
+            , x = 0
+            , y = -0.15
+            , xref='paper'
+            , yref='paper' 
+            , xanchor='left'
+            , yanchor='bottom'
+            , xshift=-1
+            , yshift=-5
+            , font=dict(size=10, color="grey")
+            , align="left"
+            ,)
+      except:
+         print("Cette figure n'a pas de source")
+
+   self.fig.update_layout(
+   font_family="News Cycle",
+   font_size=12,
+   font_color="black",
+   title_font_family="News Cycle",
+   title_font_color="black",
+   legend_title_font_color="black"
+   )
+      
+
+
+
